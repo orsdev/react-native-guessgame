03f893f03ba2333e39f6eb43be166a1d50f8fc41
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-08-24 18:31:02 +0100

    unused import removed

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, Button } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import * as Font from 'expo-font'
 import AppLoading from 'expo-app-loading';
 
